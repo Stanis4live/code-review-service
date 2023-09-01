23d8aa55303a9ed752fdbd45fbd7cd721dc33eaa
stas <chousetheone@gmail.com>
2023-09-01 21:06:15 +0300

    create files project

--- code_review_service/settings.py
+++ code_review_service/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'apps.users',
+    "apps.users",
+    "apps.files",
 ]
 
 MIDDLEWARE = [
